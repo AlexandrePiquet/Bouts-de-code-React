5d21103a3e6f2447d19ca5ceaaa7fe422fb7a32f
Alexandre Piquet <52627275+AlexandrePiquet@users.noreply.github.com>
2026-09-05 01:03:26 +0200

    Create Table.jsx

---
+++ squelette/components/Table.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import { Thead } from "./Thead";
+import { Tbody } from "./Tbody";
+import { Tfoot } from "./Tfoot";
+
+export const Table = ({ theadContent, tbodyContent, tfootContent}) => {
+
+  // render
+
+  return (
+        <table>
+          <Thead>
+
+          </Thead>
+          <Tbody>
+
+          </Tbody>
+          <Tfoot>
+            
+          </Tfoot>
+        </table>
+  );
+};
